696b60ded8bf5c6514795230f5dbfaa5bd548556
Romain Porte <rojo@gmx.fr>
2011-07-08 16:56:24 -0700

    Deleting the fg namespace

--- termcolor.hpp
+++ termcolor.hpp
@@ -29,20 +29,15 @@ namespace tc
 	const std::string underline = "\033[04m";
 	const std::string blink = "\033[05m";
 
-	// Containing all the foreground colors
-	namespace fg
-	{
-		const std::string black = "\033[30m";
-		const std::string red = "\033[31m";
-		const std::string green = "\033[32m";
-		const std::string orange = "\033[33m";
-		const std::string blue = "\033[34m";
-		const std::string magenta = "\033[35m";
-		const std::string cyan = "\033[36m";
-		const std::string white = "\033[37m";
-	}
+	const std::string black = "\033[30m";
+	const std::string red = "\033[31m";
+	const std::string green = "\033[32m";
+	const std::string orange = "\033[33m";
+	const std::string blue = "\033[34m";
+	const std::string magenta = "\033[35m";
+	const std::string cyan = "\033[36m";
+	const std::string white = "\033[37m";
 
-	// Containing all the background colors
 	namespace bkg
 	{
 		const std::string black = "\033[40m";
